6b85b5de9d4608f33aaf2f0010496dbbfb0503d5
ssuchichan <ssuchichan@163.com>
2019-01-30 13:31:10 +0800

    归并排序

--- 4recursion_tracing_partition/warmup.go
+++ 4recursion_tracing_partition/warmup.go
@@ -3,7 +3,7 @@ package rtp
 // 已知两个已排序数组，将这两个数组合并为一个排序数组。
 // [2,5,8,20] [1,3,5,7,30,50]
 // [1,2,3,5,5,7,8,20,30,50]
-func MergeArray(nums1, nums2 []int) []int {
+func MergeTwoSortedArray(nums1, nums2 []int) []int {
 	result := make([]int, 0)
 	var i, j int
 	for i, j = 0, 0; i < len(nums1) && j < len(nums2); {
@@ -25,4 +25,34 @@ func MergeArray(nums1, nums2 []int) []int {
 	}
 
 	return result
+}
+
+// 归并排序
+func MergeSort(nums []int) []int {
+	if len(nums) < 2 { // 子问题足够小时，直接求解
+		return nums
+	}
+	// 对原数组拆分了两个规模相同的数组，在对它们分别求解
+	mid := len(nums) / 2
+	// 对拆分后的两个子问题求解
+	left := MergeSort(nums[:mid])
+	right := MergeSort(nums[mid:])
+	// 将子问题的解进行合并
+	return mergeSortTwoArray(left, right)
+}
+
+func mergeSortTwoArray(left, right []int) (result []int) {
+	i, j := 0, 0
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+	return
 }
--- 4recursion_tracing_partition/warmup_test.go
+++ 4recursion_tracing_partition/warmup_test.go
@@ -8,10 +8,16 @@ import (
 func TestMergeArray(t *testing.T) {
 	nums1 := []int{2, 5, 8, 20}
 	nums2 := []int{1, 3, 5, 7, 30, 50}
-	result := MergeArray(nums1, nums2)
+	result := MergeTwoSortedArray(nums1, nums2)
 	fmt.Println(result) //[1,2,3,5,5,7,8,20,30,50]
 	nums1 = []int{1, 1, 2, 3, 5, 10, 400}
 	nums2 = []int{4, 4, 7, 8}
-	result = MergeArray(nums1, nums2)
+	result = MergeTwoSortedArray(nums1, nums2)
 	fmt.Println(result) //[1,1,2,3,4,4,5,7,8,10,400]
+}
+
+func TestMergeSort(t *testing.T) {
+	nums := []int{4, 9, 1, 19, 80, 34, 67, 22, 504, 402, 116}
+	res := MergeSort(nums)
+	fmt.Println(res)
 }
