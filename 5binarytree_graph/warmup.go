494e8dc37a6ba1181608aadd204be0e94408ce92
ssuchichan <ssuchichan@163.com>
2019-01-31 15:20:19 +0800

    遍历二叉树，记录所有路径

--- 5binarytree_graph/warmup.go
+++ 5binarytree_graph/warmup.go
@@ -2,6 +2,7 @@ package bg
 
 import "fmt"
 
+// 先序打印二叉树
 func PreorderPrint(node *TreeNode, layer int) {
 	if node == nil {
 		return
@@ -12,4 +13,17 @@ func PreorderPrint(node *TreeNode, layer int) {
 	fmt.Println(node.val)
 	PreorderPrint(node.left, layer+1)
 	PreorderPrint(node.right, layer+1)
+}
+
+// 遍历二叉树，记录从根节点到叶节点的所有路径
+func Traverse(node *TreeNode, path []*TreeNode, result *[][]*TreeNode) {
+	if node == nil {
+		return
+	}
+	path = append(path, node)                  // 将当前节点加入路径
+	if node.left == nil && node.right == nil { // 叶节点
+		*result = append(*result, path)
+	}
+	Traverse(node.left, path, result)
+	Traverse(node.right, path, result)
 }
--- 5binarytree_graph/warmup_test.go
+++ 5binarytree_graph/warmup_test.go
@@ -1,6 +1,7 @@
 package bg
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -19,5 +20,38 @@ func TestNewTreeNode(t *testing.T) {
 	b.left = e
 	b.right = f
 	PreorderPrint(root, 0)
+}
 
+func TestTraverse(t *testing.T) {
+	root := NewTreeNode(0)
+	a := NewTreeNode(1)
+	b := NewTreeNode(2)
+	c := NewTreeNode(3)
+	d := NewTreeNode(4)
+	e := NewTreeNode(5)
+	f := NewTreeNode(6)
+	g := NewTreeNode(7)
+	h := NewTreeNode(8)
+	i := NewTreeNode(9)
+	j := NewTreeNode(10)
+	root.left = a
+	root.right = b
+	a.left = c
+	a.right = d
+	b.left = e
+	b.right = f
+	d.left = g
+	e.right = h
+	f.left = i
+	f.right = j
+	path := make([]*TreeNode, 0)
+	result := make([][]*TreeNode, 0)
+	Traverse(root, path, &result)
+	for _, nodes := range result {
+		fmt.Printf("[ ")
+		for _, node := range nodes {
+			fmt.Printf("%d ", node.val)
+		}
+		fmt.Printf("]\n")
+	}
 }
