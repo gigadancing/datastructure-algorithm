34e7d74dd4a1e9fbc10d73f468b9971630b45562
ssuchichan <ssuchichan@163.com>
2019-01-31 14:11:07 +0800

    寻找最近公共祖先

--- 5binarytree_graph/binarytree_graph.go
+++ 5binarytree_graph/binarytree_graph.go
@@ -56,3 +56,57 @@ func preorder(node *TreeNode, pathValue, sum int, path []int, result *[][]int) {
 	path = path[:len(path)-1] // 将当前节点从路径中删除
 	return
 }
+
+// 例2. 已知二叉树，求二叉树中给定的两个节点的最近公共祖先。
+// 最近公共祖先：两节点v与w的最近公共祖先u，满足在树上最低（离根最远）且v、w都是u的子孙。
+//                  3
+//                 / \
+//                5   1
+//               / \ / \
+//              6  2 0  8
+//                / \
+//               7   4
+// 例如： 6、4的最近公共祖先是5。
+// 思路：
+// 1. 从根节点遍历至该节点后结束。
+// 2. 将遍历过程中的节点按顺序存储起来。
+func LowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	var ancestor *TreeNode
+	path := make([]*TreeNode, 0)           // 存放搜索路径的临时栈
+	pPath := make([]*TreeNode, 0)          // p的路径
+	qPath := make([]*TreeNode, 0)          // q的路径
+	finish := 0                            // 结束标志
+	search(root, p, pPath, &pPath, finish) // 搜索p
+	path = path[:0]                        // 清空path
+	finish = 0                             // 重置结束标志
+	search(root, q, qPath, &qPath, finish) // 搜索q
+	pathLen := 0                           // 较短路径长度
+	if len(pPath) < len(qPath) {
+		pathLen = len(pPath)
+	} else {
+		pathLen = len(qPath)
+	}
+	for i := 0; i < pathLen; i++ { // 找最近相同祖先
+		if pPath[i] == qPath[i] {
+			ancestor = pPath[i]
+		}
+	}
+	return ancestor
+}
+
+func search(current, target *TreeNode, path []*TreeNode, result *[]*TreeNode, finish int) {
+	if current == nil || finish == 1 { // 当前节点为空或已找到节点，结束搜索
+		return
+	}
+	path = append(path, current) // 当前节点加入到路径中
+	if current == target {       // 搜索到目标节点，将目标节点加入路径，停止搜索（finish置为1）
+		finish = 1
+		*result = append(*result, path...)
+	}
+	search(current.left, target, path, result, finish)  // 搜索当前节点的左孩子
+	search(current.right, target, path, result, finish) // 搜索当前节点的右孩子
+	path = path[:len(path)-1]                           // 将当前节点从路径中删除
+}
--- 5binarytree_graph/binarytree_graph_test.go
+++ 5binarytree_graph/binarytree_graph_test.go
@@ -11,21 +11,51 @@ func TestPathSum(t *testing.T) {
 	c := NewTreeNode(8)
 	d := NewTreeNode(11)
 	e := NewTreeNode(13)
-	g := NewTreeNode(4)
-	h := NewTreeNode(7)
-	i := NewTreeNode(2)
-	j := NewTreeNode(5)
-	k := NewTreeNode(1)
+	f := NewTreeNode(4)
+	g := NewTreeNode(7)
+	h := NewTreeNode(2)
+	i := NewTreeNode(5)
+	j := NewTreeNode(1)
 	a.left = b
 	a.right = c
 	b.left = d
-	d.left = h
-	d.right = i
+	d.left = g
+	d.right = h
 	c.left = e
-	c.right = g
-	g.left = j
-	g.right = k
+	c.right = f
+	f.left = i
+	f.right = j
 
 	result := PathSum(a, 22)
 	fmt.Println(result)
+}
+
+func TestLowestCommonAncestor(t *testing.T) {
+	a := NewTreeNode(3)
+	b := NewTreeNode(5)
+	c := NewTreeNode(1)
+	d := NewTreeNode(6)
+	e := NewTreeNode(2)
+	f := NewTreeNode(0)
+	g := NewTreeNode(8)
+	h := NewTreeNode(7)
+	i := NewTreeNode(4)
+	a.left = b
+	a.right = c
+	b.left = d
+	b.right = e
+	c.left = f
+	c.right = g
+	e.left = h
+	e.right = i
+	ancestor := LowestCommonAncestor(a, d, i)
+	fmt.Printf("%d和%d的公共祖先是:%d\n", d.val, i.val, ancestor.val)
+	ancestor = LowestCommonAncestor(a, h, i)
+	fmt.Printf("%d和%d的公共祖先是:%d\n", h.val, i.val, ancestor.val)
+	ancestor = LowestCommonAncestor(a, b, f)
+	fmt.Printf("%d和%d的公共祖先是:%d\n", b.val, f.val, ancestor.val)
+	ancestor = LowestCommonAncestor(a, f, g)
+	fmt.Printf("%d和%d的公共祖先是:%d\n", f.val, g.val, ancestor.val)
+	ancestor = LowestCommonAncestor(a, b, g)
+	fmt.Printf("%d和%d的公共祖先是:%d\n", b.val, g.val, ancestor.val)
 }
