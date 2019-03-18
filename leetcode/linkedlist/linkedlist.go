f6f89874ed938b5e2dc764a7185d50fbd6074daf
ssuchichan <ssuchichan@163.com>
2019-03-18 15:15:57 +0800

    判断链表是否有环，若有则返回环的起始节点；否则返回nil

--- leetcode/linkedlist/linkedlist.go
+++ leetcode/linkedlist/linkedlist.go
@@ -68,7 +68,7 @@ func MergeTwoLists2(l1, l2 *ListNode) *ListNode {
 	}
 }
 
-// 141. Linked List Cycle
+// 141. Linked List Cycle I
 // Given a linked list, determine if it has a cycle in it.
 // To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the
 // linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
@@ -117,4 +117,53 @@ func HasCycle2(head *ListNode) bool {
 		}
 	}
 	return false
+}
+
+// 142.Linked List Cycle II
+// Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+//
+// To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the
+// linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+// Note: Do not modify the linked list.
+//
+// Example 1:
+// Input: head = [3,2,0,-4], pos = 1
+// Output: tail connects to node index 1
+// Explanation: There is a cycle in the linked list, where tail connects to the second node.
+//
+// Example 2:
+// Input: head = [1,2], pos = 0
+// Output: tail connects to node index 0
+// Explanation: There is a cycle in the linked list, where tail connects to the first node.
+//
+// Example 3:
+// Input: head = [1], pos = -1
+// Output: no cycle
+// Explanation: There is no cycle in the linked list.
+func DetectCycle(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	fast, slow := head, head
+	for fast != nil {
+		if fast.Next == nil {
+			return nil
+		}
+		fast = fast.Next.Next // 走两步
+		slow = slow.Next      // 走一步
+		if fast == slow {     // 相遇
+			break
+		}
+	}
+
+	if fast == nil {
+		return nil
+	}
+
+	fast = head
+	for fast != slow {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	return fast
 }
--- leetcode/linkedlist/linkedlist_test.go
+++ leetcode/linkedlist/linkedlist_test.go
@@ -70,3 +70,41 @@ func TestHasCycle2(t *testing.T) {
 	head := HasCycle2(&n1)
 	fmt.Println(head)
 }
+
+func TestDetectCycle(t *testing.T) {
+	n1 := ListNode{Val: 3}
+	n2 := ListNode{Val: 2}
+	n3 := ListNode{Val: 0}
+	n4 := ListNode{Val: 4}
+	n1.Next = &n2
+	n2.Next = &n3
+	n3.Next = &n4
+	n4.Next = &n2
+	meet := DetectCycle(&n1)
+	if meet != nil {
+		fmt.Println(meet.Val)
+	} else {
+		fmt.Println("no cycle")
+	}
+
+	n5 := ListNode{Val: 1}
+	n6 := ListNode{Val: 2}
+	n5.Next = &n6
+	n6.Next = &n5
+	meet = DetectCycle(&n5)
+	if meet != nil {
+		fmt.Println(meet.Val)
+	} else {
+		fmt.Println("no cycle")
+	}
+
+	n7 := ListNode{Val: 1}
+	n8 := ListNode{Val:2}
+	n7.Next = &n8
+	meet = DetectCycle(&n7)
+	if meet != nil {
+		fmt.Println(meet.Val)
+	} else {
+		fmt.Println("no cycle")
+	}
+}
