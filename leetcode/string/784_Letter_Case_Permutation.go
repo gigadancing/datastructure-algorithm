b9508bc7a6db89c3501e6684baa41394e053147e
ssuchichan <ssuchichan@163.com>
2019-03-22 16:56:11 +0800

    expression add operators

---
+++ leetcode/string/784_Letter_Case_Permutation.go
@@ -0,0 +1,36 @@
+package string
+
+// 784.Letter Case Permutation
+// Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.
+// Return a list of all possible strings we could create.
+//
+// Examples:
+// Input: S = "a1b2"
+// Output: ["a1b2", "a1B2", "A1b2", "A1B2"]
+//
+// Input: S = "3z4"
+// Output: ["3z4", "3Z4"]
+//
+// Input: S = "12345"
+// Output: ["12345"]
+//
+// Note:
+//      S will be a string with length between 1 and 12.
+//      S will consist only of letters or digits.
+func letterCasePermutation(S string) []string {
+	if len(S) < 1 || len(S) > 12 {
+		return nil
+	}
+	res := make([]string, 0)
+	res = append(res, S)
+
+	for i := 0; i < len(S); i++ {
+		if S[i] >= 'A' && S[i] <= 'Z' {
+
+		} else if S[i] >= 'a' && S[i] <= 'z' {
+
+		}
+	}
+
+	return res
+}
